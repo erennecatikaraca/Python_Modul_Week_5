db3d4723b3c7b2c9628d7d76bbfb1181368f3ead
Eren <karaca.en93@gmail.com>
2025-01-13 19:10:31 +0100

    solution_1 has been completed

---
+++ solutions.py
@@ -0,0 +1,23 @@
+# solution_1
+class Rectangle:
+    def __init__(this, width, height):
+        this.witdh = width
+        this.height = height
+
+    def area(this):
+        area = this.witdh*this.height
+        return area
+    def perimeter(this):
+        perimeter = 2*(this.witdh+this.height)
+        return perimeter
+    
+rectangle = Rectangle(5,7)
+print(f"the area of the rectangle: {rectangle.area()}\nthe perimeter of the rectangle: {rectangle.perimeter()}")
+
+# solution_2
+
+# solution_3
+
+# solution_4
+
+# solution_5
